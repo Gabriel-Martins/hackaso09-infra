79e6c380201c7bf43ded8feb83b1145a90dd4b9e
Gabriel-Martins <GMartinsss@hotmail.com>
2022-09-24 14:13:58 -0300

    artifact registry

--- gpc-main.tf
+++ gpc-main.tf
@@ -1,43 +1,13 @@
 resource "google_artifact_registry_repository" "frontend-repo" {
   location = "us-east1"
-  repository_id = "hacka_09_frontend-repo"
+  repository_id = "hacka-09-frontend-repo"
   description = "Registry to store images of the front end application"
   format = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "backend-repo" {
   location = "us-east1"
-  repository_id = "hacka_09_backend-repo"
+  repository_id = "hacka-09-backend-repo"
   description = "Registry to store images of the back end application"
   format = "DOCKER"
-}
-
-resource "google_sql_database_instance" "instance2" {
-  name = "spotmusicinstance2"
-  database_version = "MYSQL_8_0"
-  region = "us-east1"
-  settings {
-    tier = "db-f1-micro"
-  }
-}
-
-resource "google_sql_database" "database" {
-  name = "playlist"
-  instance = "{google_sql_database_instance.instance2.name}"
-  charset = "utf8"
-  collation = "utf8_general_ci"
-}
-
-resource "google_sql_user" "users" {
-  name = "root"
-  instance = "{google_sql_database_instance.instance2.name}"
-  host = "%"
-  password = "mypassw0rd"
-}
-
-resource "google_sql_user" "playuser" {
-  name = "playuser"
-  instance = "{google_sql_database_instance.instance2.name}"
-  host = "%"
-  password = "123456"
 }
