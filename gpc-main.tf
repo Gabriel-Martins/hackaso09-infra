66399934539d9f51bca0d3993cc2b80bda2d2629
Gabriel-Martins <GMartinsss@hotmail.com>
2022-09-24 14:20:04 -0300

    mysql

--- gpc-main.tf
+++ gpc-main.tf
@@ -11,3 +11,19 @@ resource "google_artifact_registry_repository" "backend-repo" {
   description = "Registry to store images of the back end application"
   format = "DOCKER"
 }
+
+resource "google_sql_database_instance" "instance3" {
+  name = "spotmusicinstance3"
+  database_version = "MYSQL_8_0"
+  region = "us-east1"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "database" {
+  name = "playlist"
+  instance = "{google_sql_database_instance.instance3.name}"
+  charset = "utf8"
+  collation = "utf8_general_ci"
+}
